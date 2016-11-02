9d16102ff1242f9efed6bc2b4de8e89a55c87dc0
John Ngo <jngo@revspringinc.com>
2016-11-02 14:00:38 -0700

    get commit

---
+++ nodecmd/cmdconfig.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.file_exe_fullpath = 'C:\\dev\\github\\revspring\\Obsidian.NETMainSolution\\RemitScript\\bin\\x64\\x64\\RemitScript.exe';
+config.file_exe_args_jsonObj = '';
+
+module.exports = config;
